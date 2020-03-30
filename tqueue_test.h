f5c5d4ffbcfafd08ad7f5730a6bb20cdb666bda0
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-03-30 12:32:09 +0200

    enhanced test

--- tqueue_test.h
+++ tqueue_test.h
@@ -4,8 +4,10 @@
 
 #ifndef SISTEMI_OPERATIVI_TQUEUE_TEST_H
 #define SISTEMI_OPERATIVI_TQUEUE_TEST_H
+
 void test_tqueue_push_pop();
 void test_tqueue_size();
 void test_tqueue_get_data();
 void test_tqueue_offset_at();
+
 #endif //SISTEMI_OPERATIVI_TQUEUE_TEST_H
